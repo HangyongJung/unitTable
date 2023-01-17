ea0c90455a548d454046d01b3662b2fadef8c7ad
gkddyd <gkddyd@naver.com>
2023-01-17 11:26:27 +0900

    google

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { SWRConfig } from 'swr'
 import axios from 'axios'
 import Head from 'next/head'
 import SideBar from '../components/SideBar'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api"
@@ -26,7 +27,24 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-2LC2GCG0ZN`}></Script>
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6B9TTPEHY2', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}></Script>
       <Head>
+        <meta name="google-site-verification" content="luvYicf9TzmoZtUQ0daUNFpIFgPFmklTkJlIgJc_ha4" />
       </Head>
       <SWRConfig value={{ fetcher }}>
         {/* {!authRoute && <NavBar />} */}
