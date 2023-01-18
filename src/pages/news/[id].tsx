f5e2c929cd18f45a51012ffbfca5b46dff031f95
gkddyd <gkddyd@naver.com>
2023-01-19 00:08:56 +0900

    news

---
+++ src/pages/news/[id].tsx
@@ -0,0 +1,30 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+import useSWR from 'swr'
+
+interface News {
+  title: string
+  article: string
+  imgurl: string
+}
+const News = () => {
+  const router = useRouter()
+  const { id } = router.query
+  const { data: news, error, mutate: mutate } = useSWR<News>(id ? `/news/${id}` : null)
+
+  return (
+    <div className="bg-white py-24 sm:py-32 lg:py-40">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="sm:text-center">
+          <h2 className="text-lg font-semibold leading-8 text-indigo-600">Korean News</h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{news.title}</p>
+          <p className="mx-auto mt-6 max-w-2xl text-lg leading-8 text-gray-600">
+            {news.article}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default News
