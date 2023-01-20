28542c18b5d7b389285f625f8f0b0cfb1220873b
gkddyd <gkddyd@naver.com>
2023-01-20 13:51:47 +0900

    robot

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'http://heonapage.store',
+  siteUrl: process.env.SITE_URL || 'http://heonpage.store',
   generateRobotsTxt: true, // (optional)
   // ...other options
 }
