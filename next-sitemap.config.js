08301926ca38a73de5a9fa5efa408da91bac62e2
gkddyd <gkddyd@naver.com>
2023-01-20 13:48:37 +0900

    sitempa

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'http://heonapge.store',
+  siteUrl: process.env.SITE_URL || 'http://heonapage.store',
   generateRobotsTxt: true, // (optional)
   // ...other options
 }
